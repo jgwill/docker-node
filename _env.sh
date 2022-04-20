89fccb50e25fdedd6b1ba5d31bfc907f9d3e4447
Jean Guillaume <jgi@jgwill.com>
2022-04-20 04:33:16 -0400

    add:pl

--- _env.sh
+++ _env.sh
@@ -1,3 +1,9 @@
-export containertag=jgwill/node:16.14.2
-export dockertag=$containertag
+export containertag="jgwill/node:16.14.2"
+export dockertag="$containertag"
+export containername="tstjgwillnode1614x"
+
 #export containertag=jgwill/node:latest
+#dkecho=true
+dkhostname="$containername"
+
+dkcommand=bash #command to execute
