d886f8f31383cc892b15579e589f1513b6b3c06c
Jean Guillaume <jgi@jgwill.com>
2022-05-10 13:09:32 -0400

    up

--- _env.sh
+++ _env.sh
@@ -1,6 +1,6 @@
-export containertag="jgwill/node:16.14.2"
+export containertag="jgwill/node:16.15"
 export dockertag="$containertag"
-export containername="tstjgwillnode1614x"
+export containername="tstjgwillnode1615x"
 
 #export containertag=jgwill/node:latest
 #dkecho=true
